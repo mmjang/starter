c5ea95cd384199c367d11c6eea19f31457f573b9
mmjang <752515918@qq.com>
2023-05-03 23:36:00 +0800

    prettier and gruvbox

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,6 +1,15 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+if true then return {
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+  { "numToStr/Comment.nvim" }
+} end
 
 -- every spec file under config.plugins will be loaded automatically by lazy.nvim
 --
